cefca072d84683de5181733aefce9b1f9f626c95
Carlos Rony <carlosrecla03@gmail.com>
2021-08-24 18:18:23 -0300

    resolução do exercicio

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace olamundonome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ola Usuario,Carlos");
+        }
+    }
+}
